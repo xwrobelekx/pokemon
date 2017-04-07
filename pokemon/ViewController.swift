3120aa72878d896e7bbe6db37660a976a28052ab
Kamil Wrobel <xwrobelekx@Ewas-MBP.home>
2017-04-07 15:34:39 -0400

    now we can track location, and then we can browse the map

--- pokemon/ViewController.swift
+++ pokemon/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    
+    
+    
+    // this var gives us acces to location, so we can do some work with it later
     var locationManager = CLLocationManager()
+    
+    // this variable will allow as later to control the locati on map update.
+    var updateCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,19 +32,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             print("ready to go")
             mapView.showsUserLocation = true
+            
+            // this code gives your current location
+            
+            locationManager.startUpdatingLocation()
+            
+            
         } else {
             locationManager.requestWhenInUseAuthorization()
         }
-        
-       
-        
-        
-        
         
         
     }
     
     
+    //this function tell the delegate that the location was updated, and that there is a new location avaliable
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("we made it")
+        
+        if updateCount < 7 {
+        
+        // this region constant takes your curent location, and then u set up how much the map should be zoomed in
+        let region = MKCoordinateRegionMakeWithDistance(locationManager.location!.coordinate, 1000, 1000)
+        
+        // this sshows your current location on map zoomed in when app is lunched
+        mapView.setRegion(region, animated: false)
+            
+            updateCount += 1
+        }
+    }
     
     
     
